8a99ab116e8eadf32db9a53cc16dc54518e3ab83
JShweiri <wafflemanz3000@gmail.com>
2022-06-02 18:29:37 -0700

    performance fixes

--- LED-Matrix-Grad.ino
+++ LED-Matrix-Grad.ino
@@ -2,7 +2,7 @@
 //Define a header with all characters, heart, ucr logo, images...
 
 //PWM Quanta
-#define PWM_SIZE 16.0
+#define PWM_SIZE 10
 
 // [Y][X][R/G/B]
 uint8_t BUF[32][32][3];
@@ -30,11 +30,11 @@ void setup() {
 }
 
 void loop() {
-    int x = 32.0 * rand() / RAND_MAX;
-    int y = 32.0 * rand() / RAND_MAX;
-    BUF[y][x][RED] = PWM_SIZE * rand() / RAND_MAX;
-    BUF[y][x][BLUE] = PWM_SIZE * rand() / RAND_MAX;
-    BUF[y][x][GREEN] = PWM_SIZE * rand() / RAND_MAX;
+    int x = rand() % 32;
+    int y = rand() % 32;
+    BUF[y][x][RED] = rand() % PWM_SIZE;
+    BUF[y][x][BLUE] = rand() % PWM_SIZE;
+    BUF[y][x][GREEN] = rand() % PWM_SIZE;
 
     delayWhileDisplaying(33);
 }
@@ -49,38 +49,35 @@ inline void delayWhileDisplaying(long period){
 inline void sendBuffer() {
 
     // PWM counter 0-PWM_SIZE
-    //        for(uint8_t PWM=0;PWM<PWM_SIZE;PWM++){
+for(uint8_t PWM=0;PWM<PWM_SIZE;++PWM){
 
-    for (uint8_t line = 0; line < 16; line++) {
-
-        digitalWrite(OE, 1); // Turn off output (maybe remove this if too dim)
-
-        // somehow all 1s
-        digitalWrite(A, ((line & 1) > 0) ? 1 : 0);
-        digitalWrite(B, ((line & 2) > 0) ? 1 : 0);
-        digitalWrite(C, ((line & 4) > 0) ? 1 : 0);
-        digitalWrite(D, ((line & 8) > 0) ? 1 : 0);
-
+    for (uint8_t line = 0; line < 16; ++line) {
+        
         // Send 2 rows of color data
         uint8_t x;
-        for (x = 0; x < 32; x++) {
-            digitalWrite(R1, F_BUF_R[line][x] > PWM); // Output each color data to the pin
-            digitalWrite(R2, F_BUF_R[line + 16][x] > PWM); // Dot brightness is brighter than the current PWM counter
-            digitalWrite(G1, F_BUF_G[line][x] > PWM); // 1 when bright,
-            digitalWrite(G2, F_BUF_G[line + 16][x] > PWM); // Output 0 when it is dark
-            digitalWrite(B1, F_BUF_B[line][x] > PWM);
-            digitalWrite(B2, F_BUF_B[line + 16][x] > PWM);
+        for (x = 0; x < 32; ++x) {
+            digitalWrite(R1, BUF[line][x][RED] > PWM); // Output each color data to the pin
+            digitalWrite(R2, BUF[line + 16][x][RED] > PWM); // Dot brightness is brighter than the current PWM counter
+            digitalWrite(G1, BUF[line][x][GREEN] > PWM); // 1 when bright,
+            digitalWrite(G2, BUF[line + 16][x][GREEN] > PWM); // Output 0 when it is dark
+            digitalWrite(B1, BUF[line][x][BLUE] > PWM);
+            digitalWrite(B2, BUF[line + 16][x][BLUE] > PWM);
             digitalWrite(CLK, 1); // Send one clock to shift the data
             digitalWrite(CLK, 0);
         }
-        
-        digitalWrite(OE, 0); // Turn on output
-        
+
+        digitalWrite(OE, 1); // Turn off output while we switch lines and latch data
+
+        digitalWrite(A, ((line & 1) > 0));
+        digitalWrite(B, ((line & 2) > 0));
+        digitalWrite(C, ((line & 4) > 0));
+        digitalWrite(D, ((line & 8) > 0));
+
         digitalWrite(LAT, 1); // Import the contents of the shift register to the latch
         digitalWrite(LAT, 0);
-        
-        delayMicroseconds(600); // to prevent ghosting 500-1000
 
-//         }
+        digitalWrite(OE, 0); // Turn on output
+        
+         }
     }
 }
